2932332554e919d33e1b6cfb838d58a3ed5d82e4
Tom Boone <tbc@tomboone.com>
2025-09-14 16:08:36 -0400

    remove cert output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,8 +6,3 @@ output "python_version" {
   value       = azurerm_linux_web_app.main.site_config[0].application_stack[0].python_version
   description = "The Python version configured for the app service"
 }
-
-output "mysql_ca_cert_content" {
-  value       = local.mysql_ca_cert_content
-  description = "Combined MySQL CA certificate content for local storage"
-}
