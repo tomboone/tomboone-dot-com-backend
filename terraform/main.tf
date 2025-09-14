626522480103b2cb1ccd73d67c6511ab48d9dabf
Tom Boone <tbc@tomboone.com>
2025-09-14 13:17:17 -0400

    upgrade python to 3.13

--- terraform/main.tf
+++ terraform/main.tf
@@ -147,7 +147,7 @@ resource "azurerm_linux_web_app" "main" {
     http2_enabled = true
     minimum_tls_version = "1.2"
     application_stack {
-      python_version = "3.12"
+      python_version = "3.13"
     }
   }
 
